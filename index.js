e30c0b1ac4491549c8a26b68701bf02da7773085
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-17 18:18:55 +0200

    add joi-objectid

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require("express")();
 const mongoose = require("mongoose");
 const config = require("config");
+const Joi = require("joi");
+
+Joi.objectId = require("joi-objectid")(Joi);
 
 db = config.get("db");
 mongoose
