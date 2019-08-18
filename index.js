e37fb067062685f1051401cb938226754cd1cb45
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-18 14:07:44 +0200

    update index.js

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const usersRouter = require("./routers/users");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 
 require("./startup/db")();
 
 app.use(express.json());
+app.use("/api/users", usersRouter);
 
 const port = process.env.PORT || 3000;
-const server = app.listen(port, () => console.log("listening on port", port));
+app.listen(port, () => console.log("listening on port", port));
