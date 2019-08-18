491b70a7cc1c1fb1675ccac4134c12ceb15cab62
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-18 15:53:28 +0200

    update users router

--- routers/users.js
+++ routers/users.js
@@ -3,19 +3,25 @@ const router = express.Router();
 const { User, validate } = require("../models/User");
 
 router.post("/", async (req, res) => {
-  const error = validate(req.body);
-  if (error) return res.send(400, error.details[0].message);
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-  if (User.findOne({ username: req.body.username }))
-    return res.send(400, "Username alreay registered");
+  if (!User.findOne({ username: req.body.username }))
+    return res.status(400).send("Username alreay registered");
 
   const user = new User({
     username: req.body.username,
     password: req.body.password,
-    roles: []
+    roles: req.body.roles
   });
 
-  await user.save();
+  try {
+    await user.save();
+  } catch (e) {
+    console.log(e);
+  }
 
-  res.send(201, user);
+  return res.status(201).send(user);
 });
+
+module.exports = router;
