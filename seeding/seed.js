23871966c079c223411d76c3656fd6c6b1d3247b
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-20 22:01:58 +0200

    update the seed method

--- seeding/seed.js
+++ seeding/seed.js
@@ -1,6 +1,9 @@
 const { Contract } = require("../models/Contract");
 const { Job } = require("../models/Job");
 const { Nationality } = require("../models/Nationality");
+const { Department } = require("../models/Department");
+const { Gender } = require("../models/Gender");
+const { EmployeeStatus } = require("../models/EmployeeStatus");
 const { User } = require("../models/User");
 const { Employee } = require("../models/Employee");
 
@@ -8,11 +11,17 @@ async function seed() {
   await new Contract({ name: "Normal" }).save();
   await new Job({ name: "Developer" }).save();
   await new Nationality({ name: "Sudanese" }).save();
+  await new Department({ name: "IT" }).save();
+  await new Gender({ name: "Male" }).save();
+  await new EmployeeStatus({ name: "Normal" }).save();
+  await new Nationality({ name: "Sudanese" }).save();
   await new User({
     username: "alkhatim",
     password: "12345",
     roles: ["admin"]
   }).save();
+
+  console.log("all done...");
 }
 
 module.exports = seed;
