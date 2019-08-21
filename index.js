1114e35af8dc2149d05478008129a3b8cb23bdeb
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-21 16:56:51 +0200

    add employees router to index.js

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const usersRouter = require("./routes/users");
+const employeesRouter = require("./routes/employees");
 
 require("./startup/db")();
 
 app.use(express.json());
 app.use("/api/users", usersRouter);
+app.use("/api/employees", employeesRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("listening on port", port));
