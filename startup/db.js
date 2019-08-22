d18f9a5f0e0807ca3149cde7dabd25eb88d00796
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-22 16:52:05 +0200

    set a new global option to mongodb

--- startup/db.js
+++ startup/db.js
@@ -5,7 +5,7 @@ module.exports = function() {
   db = config.get("db");
   mongoose.set("useCreateIndex", true);
   mongoose
-    .connect(db, { useNewUrlParser: true })
+    .connect(db, { useNewUrlParser: true, useFindAndModify: false })
     .then(() => console.log("connected to", db))
     .catch(e => console.log(e));
 };
