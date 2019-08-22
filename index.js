22447913820cc852071d38cf34156cf346e2f6cb
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-22 12:39:10 +0200

    add template for calling seed in index.js

--- index.js
+++ index.js
@@ -13,3 +13,5 @@ app.use("/api/employees", employeesRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("listening on port", port));
+
+// require("./seeding/seed")();
