29249911186c0adefdbecf9f7b353cc85a1d4be4
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-22 23:45:47 +0200

    add patch verb to users router to update password

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
 const { User, validate } = require("../models/User");
+const Joi = require("joi");
+const PasswordComplexity = require("joi-password-complexity");
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -34,6 +36,31 @@ router.post("/", async (req, res) => {
     .header("x-jwt", token)
     .header("access-control-expose-headers", "x-jwt")
     .send(_.pick(user, ["_id", "username"]));
+});
+
+router.patch("/:id", async (req, res) => {
+  const schema = {
+    password: new PasswordComplexity({
+      min: 6,
+      max: 26,
+      lowerCase: 1,
+      upperCase: 1,
+      numeric: 1,
+      symbol: 0,
+      requirmentCount: 3
+    })
+  };
+  const { error } = Joi.validate(req.body, schema);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  req.body.password = await bcrypt.hash(
+    req.body.password,
+    await bcrypt.genSalt(10)
+  );
+
+  await User.findByIdAndUpdate(req.params.id, req.body);
+
+  res.status(200).send();
 });
 
 module.exports = router;
