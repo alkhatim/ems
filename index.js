d03742ed607e5d8f21cecc0cfe06b1421da64339
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-24 08:29:41 +0200

    add express-async-errors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+require("express-async-errors");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const usersRouter = require("./routes/users");
