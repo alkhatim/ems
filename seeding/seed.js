e79f88ae41d38626cad4c725172a14a84223fe6f
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-26 14:47:07 +0200

    update seed file and add employee.json

--- seeding/seed.js
+++ seeding/seed.js
@@ -9,19 +9,3 @@ async function seed() {
 }
 
 module.exports = seed;
-
-employeeJSON = {
-  name: "mohammed alkhatim",
-  genderId: "5d5e6b21cf308e1e10668be2",
-  nationalityId: "5d5e6b20cf308e1e10668be0",
-  birthday: "1996-03-26",
-  address: "Khartoum-Arkaweet",
-  phone: "0920012880",
-  bankAccount: "1796861",
-  contractId: "5d5e6b1bcf308e1e10668bde",
-  jobId: "5d5e6b20cf308e1e10668bdf",
-  departmentId: "5d5e6b21cf308e1e10668be1",
-  contractExpiryDate: "2020-01-01",
-  basicSalary: 1000,
-  foodAllowance: 200
-};
