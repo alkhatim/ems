c15802307c63c3e9abfcb1c324e40dd6b792886d
alkhatim <mohammed.alkhatim@ebanan.com>
2019-08-30 08:24:55 +0200

    rename resolved to Resolved

--- routes/deductions.js
+++ routes/deductions.js
@@ -68,7 +68,7 @@ router.get("/:id", validateObjectId, async (req, res) => {
 
 router.put("/:id", async (req, res) => {
   const currentState = await Deduction.findById(req.params.id).select("state");
-  if (currentState.name == "resolved")
+  if (currentState.name == "Resolved")
     return res
       .status(400)
       .send("You can't modify a deduction that has been resolved");
