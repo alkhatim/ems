81a8e401cb6326bc1b728c0499946c693e3c5027
alkhatim <mohammed.alkhatim@ebanan.com>
2019-09-15 15:02:40 +0200

    fix issue with auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("config");
 
 module.exports = function(req, res, next) {
-  const token = req.headers("x-jwt");
+  const token = req.header("x-jwt");
   if (!token) return res.status(401).send("Access denied");
 
   try {
