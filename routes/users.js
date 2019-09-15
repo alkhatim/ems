5d83559b8c78a4ca415e69c41701497945caabea
alkhatim <mohammed.alkhatim@ebanan.com>
2019-09-15 10:49:41 +0200

    update users router

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,8 @@ const express = require("express");
 const router = express.Router();
 const _ = require("lodash");
 const bcrypt = require("bcrypt");
-const Joi = require("joi");
-const PasswordComplexity = require("joi-password-complexity");
 const { User, validate } = require("../models/User");
+const validateObjectId = require("../middleware/validateObjectId");
 
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
@@ -27,40 +26,57 @@ router.post("/", async (req, res) => {
   await user.save();
 
   const token = user.genJwt();
+
   res
     .status(201)
-    .header("x-jwt", token)
     .header("access-control-expose-headers", "x-jwt")
+    .header("x-jwt", token)
     .send(_.pick(user, ["_id", "username"]));
 });
 
-// for changing a user's password
-router.patch("/:id", async (req, res) => {
-  const user = await User.findById(req.params.id);
+router.put("/:id", validateObjectId, async (req, res) => {
+  let user = await User.findById(req.params.id);
   if (!user) return res.status(404).send("There is no user with the given ID");
 
-  const schema = {
-    password: new PasswordComplexity({
-      min: 6,
-      max: 26,
-      lowerCase: 1,
-      upperCase: 1,
-      numeric: 1,
-      symbol: 0,
-      requirmentCount: 3
-    })
-  };
-  const { error } = Joi.validate(req.body, schema);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
+  if (
+    await User.findOne({
+      username: req.body.username,
+      _id: { $ne: req.params.id }
+    })
+  )
+    return res.status(400).send("Username alreay registered");
 
   req.body.password = await bcrypt.hash(
     req.body.password,
     await bcrypt.genSalt(10)
   );
 
-  await User.findByIdAndUpdate(req.params.id, req.body);
+  user = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      username: req.body.username,
+      password: req.body.password,
+      roles: req.body.roles
+    },
+    { new: true }
+  );
 
-  res.status(200).send();
+  const token = user.genJwt();
+
+  res
+    .status(200)
+    .header("access-control-expose-headers", "x-jwt")
+    .header("x-jwt", token)
+    .send(_.pick(user, ["_id", "username"]));
+});
+
+router.delete("/:id", validateObjectId, async (req, res) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+  if (!user) return res.status(404).send("There is no user with the given ID");
+  res.status(200).send(_.pick(user, ["_id", "username"]));
 });
 
 module.exports = router;
