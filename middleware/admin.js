a1f0fc4b930dfd387eeb8e28933054a8af3708f2
alkhatim <mohammed.alkhatim@ebanan.com>
2019-09-15 15:54:40 +0200

    add admin middleware function and rename user middleware function

--- middleware/admin.js
+++ middleware/admin.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next) {
   const roles = req.user.roles;
-  if (!roles.includes("hrUser"))
+  if (!roles.includes("admin"))
     return res.status(403).send("You don't have the required permissions");
   next();
 };
