5a75d264dcd792af6058aadaceb7c0027b182925
alkhatim <mohammed.alkhatim@ebanan.com>
2020-01-09 15:21:16 +0200

    add client app

---
+++ client/src/components/layouts/Landing.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">EMS</h1>
+          <p className="lead">Manage employees' data and salaries</p>
+          <div className="buttons">
+            <a href="register.html" className="btn btn-primary">
+              Sign Up
+            </a>
+            <a href="login.html" className="btn btn-light">
+              Login
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
