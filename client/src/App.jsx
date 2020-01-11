a9b4581caab307fe2ecb43e02b80288090c5b934
alkhatim <mohammed.alkhatim@ebanan.com>
2020-01-11 12:20:08 +0200

    update the client app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { Navbar } from "./components/layouts/Navbar";
 import { Landing } from "./components/layouts/Landing";
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.css";
-// import "materialize-css/dist/css/materialize.css";
+import "materialize-css/dist/css/materialize.css";
 
 const App = () => (
   <Fragment className="App">
--- client/src/components/layouts/Navbar.jsx
+++ client/src/components/layouts/Navbar.jsx
@@ -5,7 +5,8 @@ export const Navbar = () => {
     <nav className="navbar bg-dark">
       <h1>
         <a href="index.html">
-          <i className="fas fa-code"></i> EMS
+          <i className="far fa-id-badge" />
+          <span style={{ marginLeft: 7.5, fontSize: 25 }}>EMS</span>
         </a>
       </h1>
       <ul>
