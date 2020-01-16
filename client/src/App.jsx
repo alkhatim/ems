7c3b328c0db45a6cd05ec91dfe7515bf176cd24e
alkhatim <mohammed.alkhatim@ebanan.com>
2020-01-16 13:03:02 +0200

    move stylesheets from app.js to index.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,8 +2,6 @@ import React, { Fragment } from "react";
 import { Navbar } from "./components/layouts/Navbar";
 import { Landing } from "./components/layouts/Landing";
 import "./App.css";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "materialize-css/dist/css/materialize.css";
 
 const App = () => (
   <Fragment className="App">
--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "materialize-css/dist/css/materialize.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
