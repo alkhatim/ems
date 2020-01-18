9116c7a58223446cb2f15902ca2a18d1f5226619
alkhatim <mohammed.alkhatim@ebanan.com>
2020-01-18 09:24:44 +0200

    add routing to client app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,18 @@ import React, { Fragment } from "react";
 import { Navbar } from "./components/layouts/Navbar";
 import { Landing } from "./components/layouts/Landing";
 import { Footer } from "./components/layouts/Footer";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import "./spacing.css";
 
 const App = () => (
-  <Fragment className="App">
+  <BrowserRouter>
     <Navbar />
-    <Landing />
+    <Switch>
+      <Route exact path="/" component={Landing} />
+    </Switch>
     <Footer />
-  </Fragment>
+  </BrowserRouter>
 );
 
 export default App;
