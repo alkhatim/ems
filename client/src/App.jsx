42b4a727029236ff562da60ffa06f99a50eb14c0
alkhatim <mohammed.alkhatim@ebanan.com>
2020-01-19 15:53:13 +0200

    add toastify, change client font

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,27 @@ import { Landing } from "./components/layouts/Landing";
 import { Login } from "./components/auth/Login";
 import { Register } from "./components/auth/Register";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "materialize-css/dist/css/materialize.min.css";
 import "./App.css";
 import "./spacing.css";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const App = () => (
-  <BrowserRouter>
-    <Navbar />
-    <Switch>
-      <Route exact path="/" component={Landing} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register} />
-    </Switch>
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>
+    <ToastContainer pauseOnFocusLoss={false} />
+  </Provider>
 );
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "@fortawesome/fontawesome-free/css/all.css";
-import "materialize-css/dist/css/materialize.min.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
