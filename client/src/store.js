9cf7ca387ddbc80a826fcda7647f4bdd4d4020ae
alkhatim <mohammed.alkhatim@ebanan.com>
2020-01-19 15:55:27 +0200

    add store and rootReducer

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,14 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
-import rootReducer from "./reducers";
+import rootReducder from "./reducers/rootReducer";
+
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  rootReducder,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
