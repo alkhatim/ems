db50e3117dc9a68157823b01b44dad1e84244c55
alkhatim <mohammed.alkhatim@ebanan.com>
2020-01-28 12:02:58 +0200

    add lookups service

---
+++ client/src/services/lookups.js
@@ -0,0 +1,36 @@
+import http from "./http.js";
+import messages from "./messages";
+
+const getLookup = type => {
+  if (!Lookup[type]) fetchLookup(type);
+  return Lookup[type];
+};
+
+const fetchLookup = async type => {
+  try {
+    Lookup[type] = await http.get(`/lookups/?lookup=${type}`);
+  } catch (error) {
+    messages.error(error);
+  }
+};
+
+const Lookup = {
+  BatchType: null,
+  Contract: null,
+  DeductionType: null,
+  Department: null,
+  EmployeeStatus: null,
+  Gender: null,
+  InstallmentState: null,
+  Job: null,
+  LoanState: null,
+  Location: null,
+  MissionState: null,
+  Nationality: null,
+  OvertimeType: null,
+  State: null,
+  VacationState: null,
+  VacationType: null
+};
+
+export default getLookup;
