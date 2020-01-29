7ee1050f68c1e307042fe76efaeef257b3a87376
alkhatim <mohammed.alkhatim@ebanan.com>
2020-01-29 22:40:09 +0200

    remove dropdown control

--- client/src/components/misc/Playground.jsx
+++ client/src/components/misc/Playground.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import Dropdown from "../shared/Dropdown";
 
 export const Playground = () => {
   return (
     <div>
       <div className="container">
         <div className="row">
-          <div className="col s6 offset-s3">{/* Play here */}</div>
+          <div className="col s6 offset-s3"></div>
         </div>
       </div>
     </div>
