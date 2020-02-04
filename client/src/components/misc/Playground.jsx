171688295e3e02e9d9e3038561ff9f37d56ba6f5
alkhatim <mohammed.alkhatim@ebanan.com>
2020-02-04 18:46:59 +0200

    update autocomplete control

--- client/src/components/misc/Playground.jsx
+++ client/src/components/misc/Playground.jsx
@@ -3,12 +3,22 @@ import Dropdown from "./../shared/Dropdown";
 import Autocomplete from "./../shared/Autocomplete";
 
 export const Playground = () => {
+  let selected = "";
+  const handleSelect = id => {
+    console.log(id);
+  };
+
   return (
     <div>
       <div className="container">
         <div className="row">
           <div className="col s6 offset-s3">
-            <Autocomplete label="Employee" lookup="Employee" />
+            <Autocomplete
+              selected={selected}
+              onChose={handleSelect}
+              label="Employee"
+              lookup="Employee"
+            />
           </div>
         </div>
         <div className="row">
