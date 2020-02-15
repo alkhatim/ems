cbb83b50a5759f983661ad05658edbd179f0c35b
alkhatim <mohammed.alkhatim@ebanan.com>
2020-02-15 18:24:26 +0200

    add ActionTypes

---
+++ client/src/actions/ActionTypes.js
@@ -0,0 +1,27 @@
+// application actions
+export const LOADING = "LOADING";
+export const LOADED = "LOADED";
+
+// auth actions
+export const LOGGED_IN = "LOGGED_IN";
+export const LOGIN_FAILED = "LOGIN_FAILED";
+export const REGISTERED = "REGISTERED";
+export const REGISTER_FAILED = "REGISTER_FAILED";
+export const USER_LOADED = "USER_LOADED";
+export const USER_LOAD_FAILED = "USER_LOAD_FAILED";
+export const LOGGED_OUT = "LOGGED_OUT";
+
+// inbox actions
+export const EMPTY_INBOX = "EMPTY_INBOX";
+export const INBOX_LOADED = "INBOX_LOADED";
+export const INBOX_LOAD_FAILED = "INBOX_LOAD_FAILED";
+export const MESSAGE_READ = "MESSAGE_READ";
+export const MESSAGE_READ_FAILED = "MESSAGE_READ_FAILED";
+export const MESSAGE_LOADED = "MESSAGE_LOADED";
+export const MESSAGE_LOAD_FAILED = "MESSAGE_LOAD_FAILED";
+export const MESSAGE_SENT = "MESSAGE_SENT";
+export const MESSAGE_SEND_FAILED = "MESSAGE_SEND_FAILED";
+
+// employee actions
+export const EMPLOYEE_LOADED = "EMPLOYEE_LOADED";
+export const EMPLOYEE_LOAD_FAILED = "EMPLOYEE_LOAD_FAILED";
